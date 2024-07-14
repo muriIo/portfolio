d08e625e4a8cc6a69f0c4fd678e329b828e3a68d
Murilo Cruz <murilomecr@gmail.com>
2024-07-13 22:07:07 -0300

    feature(localize): adding locale for pt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import localePtExtra from '@angular/common/locales/extra/pt';
 
 import { routes } from './app.routes';
 
+registerLocaleData(localePt, 'pt', localePtExtra);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pt' }
+  ]
 };
